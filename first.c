15557a19462cd97290acc4e7dc8ccfe3d8d2bfc5
Bhupendra kumawat <bhupendra.12223305@lpu.in>
2023-02-14 16:13:12 +0530

    simple

--- first.c
+++ first.c
@@ -1,4 +1,34 @@
 #include<stdio.h>
 void main(){
-    printf("hello world");
+// for (int  i = 1; i <= 5; i++)
+// {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         printf("* ");
+//     }
+//     printf("\n");
+// }
+// for (int  i = 5; i >= 1; i--)
+// {
+//     for (int j = 1; j <= i; j++)
+//     {
+//         printf("* ");
+//     }
+//     printf("\n");
+// }
+// for (int  i = 1; i <= 5; i++)
+// {
+//     for (int j = 5; j >= i; j--)
+//     {
+//         printf("* ");
+//     }
+//     printf("\n");
+// }
+// int x,a,b;
+// printf("enter first number=");
+// scanf("%d %d",&a,&b);
+// x=a/b?a:b;
+// printf("%d",x);
+// a=printf("123e3432rf     ");
+// printf("\n%d",a);
 }
